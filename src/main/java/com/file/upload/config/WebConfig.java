542102e02209e049931b2d891c9186baa6857cb5
winterSwallow <735378456@qq.com>
2020-06-01 09:30:06 +0800

    设置静态资源访问路径和查询路径

---
+++ src/main/java/com/file/upload/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.file.upload.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${diskName}")
+    private String diskName;
+
+    @Value("${fileDirectory}")
+    private String fileDirectory;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/upload/**").addResourceLocations("file:" + diskName + fileDirectory);
+    }
+}
